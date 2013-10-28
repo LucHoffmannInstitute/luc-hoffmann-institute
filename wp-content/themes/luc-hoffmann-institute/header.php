e9625f71190674b08fec8f234a7671aa33d170c5
Darin Reid <darinreid@gmail.com>
2013-10-28 10:59:20 -0700

    Meta viewport uncommented

--- wp-content/themes/luc-hoffmann-institute/header.php
+++ wp-content/themes/luc-hoffmann-institute/header.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?php hoffmann_title() ?></title>
         
-        <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="title" content="<?php bloginfo('name'); ?>">
         <meta name="description" content="<?php echo get_option('meta_description'); ?>">
 
