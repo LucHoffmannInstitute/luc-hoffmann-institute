d0d3735efa59d79753d3d39eb8d3b4d70fe36a7c
Darin Reid <darinreid@gmail.com>
2014-03-11 07:14:01 -0700

    Footer search styles

--- wp-content/themes/luc-hoffmann-institute/templates/searchform.php
+++ wp-content/themes/luc-hoffmann-institute/templates/searchform.php
@@ -1,4 +1,11 @@
 <form class="Form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search this site" />
-    <button class="Button Button--action" type="submit">Search</button>
+
+	<div class="Form-input-wrap">
+
+    	<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search this site" />
+
+    	<button class="Button Button--action" type="submit"><i class="icon-search"></i><span>Search</span></button>
+
+    </div>
+
 </form>
