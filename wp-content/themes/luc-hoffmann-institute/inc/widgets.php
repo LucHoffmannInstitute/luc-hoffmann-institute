114eaef3c5aee6f6dc1ac3c6394592a9352e59ac
Darin Reid <darinreid@gmail.com>
2014-03-14 15:58:53 -0700

    Removing home sidebar

--- wp-content/themes/luc-hoffmann-institute/inc/widgets.php
+++ wp-content/themes/luc-hoffmann-institute/inc/widgets.php
@@ -16,8 +16,6 @@ function hoffmann_sidebars_init() {
 	$after_title	= '</h2></header>';
 	$after_widget	= '</section>';
 
-	$additional_sidebars = array( 'Home' );
-
 	// add project post types as additional sidebars
 	$projects = get_posts( array(
 		'post_type' => 'project'
