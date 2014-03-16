08aef4ba0ea9689e0610cd3db6cc53656f670216
Darin Reid <darinreid@gmail.com>
2014-03-16 13:20:17 -0700

    Added project filtering by url query

--- wp-content/themes/luc-hoffmann-institute/archive-project.php
+++ wp-content/themes/luc-hoffmann-institute/archive-project.php
@@ -4,13 +4,36 @@
  */
 get_header();
 
-$projects = new WP_Query( array(
-    'post_type' => 'project',
+$query = array(
+	'post_type' => 'project',
     'posts_per_page' => -1,
     'post_parent' => 0,
     'orderby' => 'menu_order',
     'order' => 'DESC'
-) );
+);
+
+$status_values = array('pending', 'current', 'completed', 'all');
+
+$status = 'current';
+
+if ( isset($_GET['status']) && in_array($_GET['status'], $status_values) )
+{
+	$status = $_GET['status'];
+}
+
+if ($status !== 'all')
+{
+
+	$query['meta_query'] = array(
+		array(
+			'key' => 'status',
+			'value' => $status
+		)
+	);
+
+}
+
+$projects = new WP_Query( $query );
 
 ?>
 
@@ -23,7 +46,7 @@ $projects = new WP_Query( array(
 				<nav class="Filter-menu Filter-menu--dark">
 					<ul>
 						<li class="Filter-menu-select">
-							<a href="#"><span>All Projects</span> <i class="icon-menu"></i></a>
+							<a href="#"><span>Current Projects</span> <i class="icon-menu"></i></a>
 						</li>
 						<li class="Filter-menu-select">
 							<a href="#"><span>All Themes</span> <i class="icon-menu"></i></a>
