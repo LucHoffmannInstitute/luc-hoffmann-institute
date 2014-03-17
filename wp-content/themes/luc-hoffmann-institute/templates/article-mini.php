1667a4a2563bd8d80fa21f30cf9a107818d254d1
Darin Reid <darinreid@gmail.com>
2014-03-17 09:32:20 -0700

    Fixing mini article styles

--- wp-content/themes/luc-hoffmann-institute/templates/article-mini.php
+++ wp-content/themes/luc-hoffmann-institute/templates/article-mini.php
@@ -5,15 +5,18 @@
 $image_id = get_field('image');
 if ( $image_id )
 {
-	$image_url = wp_get_attachment_url( $image_id );
+	$image_src = wp_get_attachment_image_src($image_id, array(100,100));
+	$image_url = $image_src[0];
 } else {
 	$image_url = get_template_directory_uri() . '/assets/img/build/apple-touch-icon-144x144-precomposed.png';
 }
 ?>
 
 <article class="Article Article--mini" id="article-<?php the_ID() ?>">
-					
-	<div class="Article-thumbnail" style="background-image: url(<?php echo $image_url ?>);"></div>
+
+	<div class="Article-thumbnail">
+		<img src="<?php echo $image_url ?>" alt="">
+	</div>
 
 	<header class="Article-header">
 
