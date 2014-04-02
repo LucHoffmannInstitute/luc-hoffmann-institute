97ef1ee70189dc9e5415f331d61dc5ba504830a3
Darin Reid <darinreid@gmail.com>
2014-04-02 10:55:12 -0700

    Adding colors to project cards

--- wp-content/themes/luc-hoffmann-institute/templates/project.php
+++ wp-content/themes/luc-hoffmann-institute/templates/project.php
@@ -3,6 +3,10 @@
  * Single project
  */
 $banner = new Banner();
+
+$work_streams = get_the_terms(get_the_ID(), 'work_streams');
+$work_stream = array_pop($work_streams);
+$color = get_field('color', $work_stream->taxonomy . '_' . $work_stream->term_id);
 ?>
 
 <article class="Project Project--list">
@@ -11,7 +15,7 @@ $banner = new Banner();
 
 		<a class="Project-image" href="<?php the_permalink() ?>" style="background-image: url(<?php echo $banner->url() ?>)"></a>
 
-		<div class="Project-header">
+		<div class="Project-header"<?php if (isset($color)) echo ' style="border-bottom-color: ' . $color . ';"' ?>>
 
 			<h2 class="Project-title"><?php the_title() ?></h2>
 
