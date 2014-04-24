ff87a2ac21659d49d3072e6384f5f967f3223aee
Darin Reid <darinreid@gmail.com>
2014-04-24 09:33:21 -0700

    Adding workstream association for pages (with color borders)

--- wp-content/themes/luc-hoffmann-institute/templates/page-header.php
+++ wp-content/themes/luc-hoffmann-institute/templates/page-header.php
@@ -5,14 +5,27 @@
 $page_ancestor_id = hoffmann_ancestor();
 $page_ancestor = get_post( $page_ancestor_id );
 
+$style = '';
+
 $banner = new Banner();
 
 if ( ! $banner->hasImages())
 {
 	$banner = new Banner(array('id' => $page_ancestor_id));
 }
+
+$style .= ' background-image: url(' . $banner->url() . ');';
+$style .= ' background-position: ' . $banner->position() . ';';
+
+// is this page associated with a workstream?
+$work_stream = get_term(get_field('work_stream'), 'work_streams');
+
+if ( isset($work_stream) && !empty($work_stream) ) {
+	$style .= ' border-bottom-color: ' . get_field('color', 'work_streams_' . $work_stream->term_id) . ';';
+}
+
 ?>
-<header class="Page-header Page-header--banner" style="background-image: url(<?php echo $banner->url() ?>); background-position: <?php echo $banner->position() ?>;">
+<header class="Page-header Page-header--banner" style="<?php echo $style ?>">
 
 	<div class="Page-header-inner">
 
