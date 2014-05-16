c1dc96a0ab73332574de3ef760834effbb184e4d
Darin Reid <darinreid@gmail.com>
2014-05-16 13:36:15 -0700

    Removing exposed count from projects

--- wp-content/themes/luc-hoffmann-institute/templates/project.php
+++ wp-content/themes/luc-hoffmann-institute/templates/project.php
@@ -2,7 +2,6 @@
 /**
  * Single project
  */
-global $count;
 
 $banner = new Banner();
 
@@ -19,7 +18,7 @@ $color = get_field('color', $work_stream->taxonomy . '_' . $work_stream->term_id
 
 		<div class="Project-header"<?php if (isset($color)) echo ' style="border-bottom-color: ' . $color . ';"' ?>>
 
-			<h2 class="Project-title"><?php echo $count ?>. <?php the_title() ?></h2>
+			<h2 class="Project-title"><?php the_title() ?></h2>
 		</div>
 
 		<div class="Project-content">
